07c60bf9bbf2d28559bc2f28f5a3af801a2fab90
Jan Henckens <jan@studioespresso.co>
2018-07-21 12:50:38 +0200

    Remove unused console command

--- src/Navigate.php
+++ src/Navigate.php
@@ -99,12 +99,7 @@ class Navigate extends Plugin
 
         // Add in our Twig extensions
         Craft::$app->view->registerTwigExtension(new NavigateTwigExtension());
-
-        // Add in our console commands
-        if (Craft::$app instanceof ConsoleApplication) {
-            $this->controllerNamespace = 'studioespresso\navigate\console\controllers';
-        }
-
+        
         // Register our CP routes
         Event::on(
             UrlManager::class,
