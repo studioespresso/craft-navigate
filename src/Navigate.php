6a799a6da4e2832e61d93dce0d2cf972ce38f187
Jan Henckens <jan@studioespresso.co>
2018-12-09 14:40:06 +0100

    Added plugin log

--- src/Navigate.php
+++ src/Navigate.php
@@ -24,6 +24,7 @@ use craft\base\Plugin;
 use craft\services\Plugins;
 use craft\events\PluginEvent;
 use craft\console\Application as ConsoleApplication;
+use craft\log\FileTarget;
 use craft\web\UrlManager;
 use craft\services\Elements;
 use craft\services\Fields;
@@ -99,7 +100,14 @@ class Navigate extends Plugin
 
         // Add in our Twig extensions
         Craft::$app->view->registerTwigExtension(new NavigateTwigExtension());
-        
+
+        $fileTarget = new FileTarget([
+            'logFile' => Craft::$app->path->getLogPath() . '/navigate.log', // <--- path of the log file
+            'categories' => ['navigate'] // <--- categories in the file
+        ]);
+        // include the new target file target to the dispatcher
+        Craft::getLogger()->dispatcher->targets[] = $fileTarget;
+
         // Register our CP routes
         Event::on(
             UrlManager::class,
@@ -131,46 +139,22 @@ class Navigate extends Plugin
                 $variable->set('navigate', NavigateVariable::class);
             }
         );
-
-        // Do something after we're installed
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                    // We were just installed
-                }
-            }
-        );
-
-/**
- * Logging in Craft involves using one of the following methods:
- *
- * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
- * Craft::info(): record a message that conveys some useful information.
- * Craft::warning(): record a warning message that indicates something unexpected has happened.
- * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
- */
-        Craft::info(
-            Craft::t(
-                'navigate',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
     }
 
+    public static function info($message)
+    {
+        Craft::getLogger()->log($message, \yii\log\Logger::LEVEL_INFO, 'navigate');
+    }
+
+    public static function warning($message)
+    {
+        Craft::getLogger()->log($message, \yii\log\Logger::LEVEL_WARNING, 'navigate');
+    }
+
+    public static function error($message)
+    {
+        Craft::getLogger()->log($message, \yii\log\Logger::LEVEL_ERROR, 'navigate');
+    }
 
     /**
      * @inheritdoc
